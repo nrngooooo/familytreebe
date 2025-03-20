b56eb06fae4fdee311351c1ffd3dd593b2b6584c
nrngooooo <is21d005@mandakh.edu.mn>
2025-03-20 17:46:14 +0800

    set db but didnt connectt

--- familytreebe/settings.py
+++ familytreebe/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from neomodel import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'django_neomodel',
     'familyapp',
 ]
 # config db
 config.DATABASE_URL = "bolt://neo4j:12345678@localhost:7687"
+NEOMODEL_FORCE_TIMEZONE = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'familytreebe.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'familytree.db',
     }
 }
 
